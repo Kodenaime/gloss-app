97232d3f43d9bb2a7946638ae2dc095d6b790923
iamkodenaime <creativedannyk@gmail.com>
2024-12-13 22:31:40 +0100

    final commit for now

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,24 +5,26 @@ import data from '../../Data/footer.json'
 const Footer = () => {
   return (
     <div className='bg-blue py-[6%] text-white'>
-      <div className="">
+      <div className="max-w-[1200px] mx-auto flex flex-col gap-10 text-center">
 
-        <div className="">
-            <ul>
+        <div className="mb-7">
+            <ul className='flex gap-32 justify-center text-[1.2rem]'>
               {
                 data.links.map((item, index) => (
-                  <li key={index}><a href={item.href}>{ item.title }</a></li>
+                  <li className='hover:text-black transition-all duration-200 ease-linear' key={index}><a href={item.href}>{ item.title }</a></li>
                 ))
               }
             </ul>
         </div>
 
-        <div>
-          <li>{ data.contacts.phone }</li>
-          <li>{ data.contacts.email }</li>
+        <div className='mb-7 flex justify-center gap-10 items-center'>
+          <ul className="flex justify-center gap-10 text-[1.3rem] font-bold">
+            <li>{ data.contacts.phone }</li>
+            <li>{ data.contacts.email }</li>
+          </ul>
 
-          <div className="">
-            <ul>
+          <div >
+            <ul className="flex justify-center gap-5 text-[1.3rem]">
               {
                 data.medias.map((media, socialIndex) => (
                   <li key={socialIndex}><a href={media.hrefs}><i className={ media.iconClass }></i></a></li>
